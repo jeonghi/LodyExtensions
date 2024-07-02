c31fa8ea56e96e91d64d0b764ba90854d4938256
jeonghi <jpark0902@kookmin.ac.kr>
2024-07-02 11:34:50 +0900

    [Add] implement uniqued Array function

---
+++ Sources/LodyExtensions/Foundation/Collection/Collection+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  Collection+Ex.swift
+//
+//  Created by Lody on 5/8/24.
+//
+
+import Foundation
+
+public extension Collection where Element: Hashable {
+  func uniqued() -> [Element] {
+    let unique = Set(self)
+    return Array(unique)
+  }
+}
