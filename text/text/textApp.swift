d9093aa7b3aab080e0b83b284a712884365883c5
jeonghi <jpark0902@kookmin.ac.kr>
2024-07-02 11:34:50 +0900

    [Add] implement uniqued Array function

---
+++ text/text/textApp.swift
@@ -0,0 +1,17 @@
+//
+//  textApp.swift
+//  text
+//
+//  Created by 쩡화니 on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
